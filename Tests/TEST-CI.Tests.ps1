d03c1044e0c14964fdce600589efb45019dd8992
Pär Hultman <script@hultman.cc>
2016-08-30 19:44:03 +0200

    edit test

--- Tests/TEST-CI.Tests.ps1
+++ Tests/TEST-CI.Tests.ps1
@@ -21,7 +21,32 @@ Describe "$ModuleName PS$PSVersion" {
             $Module = Get-Module $ModuleName
             $Module.Name | Should be $ModuleName
             $Commands = $Module.ExportedCommands.Keys
-            $Commands -contains 'Get-BuildVariables' | Should Be $True
+            $Commands -contains 'Write-HelloWorld' | Should Be $True
+        }
+    }
+}
+
+
+Describe "Get-ProjectName PS$PSVersion" {
+    Context 'Strict mode' {
+
+        Set-StrictMode -Version latest
+
+        It 'Should pick by same name nested folder' {
+            $ProjectName = Get-ProjectName $PSScriptRoot\TestData\ProjectX
+            $ProjectName | Should Be 'ProjectX'
+        }
+        It 'Should pick by PSD1 in folder' {
+            $ProjectName = Get-ProjectName $PSScriptRoot\TestData\ProjectPSD
+            $ProjectName | Should Be 'ProjectPSD'
+        }
+        It 'Should pick by PSD1 in subfolder' {
+            $ProjectName = Get-ProjectName $PSScriptRoot\TestData\ProjectSubPSD
+            $ProjectName | Should Be 'ProjectSubPSD'
+        }
+        It 'Should pick by PSD1 in subfolder with different name' {
+            $ProjectName = Get-ProjectName $PSScriptRoot\TestData\ProjectWTF
+            $ProjectName | Should Be 'ProjectEvil'
         }
     }
 }
