b55bd6d7a5eab4559d8b065487041a974067e706
Pär Hultman <script@hultman.cc>
2016-08-30 18:36:16 +0200

    Added Gitignore

---
+++ TEST-CI/Test-CI.psm1
@@ -0,0 +1,21 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.115
+	 Created on:   	2016-08-30 18:30
+	 Created by:   	 Pär Hultman
+	 Organization: 	 
+	 Filename:     	Test-CI.psm1
+	-------------------------------------------------------------------------
+	 Module Name: Test-CI
+	===========================================================================
+#>
+
+function Write-HelloWorld
+{
+	Write-Host "Hello World"
+}
+
+Export-ModuleMember Write-HelloWorld
+
+
+
